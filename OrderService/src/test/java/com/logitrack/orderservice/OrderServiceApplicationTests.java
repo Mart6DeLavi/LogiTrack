40363155b5fe572ea5714a1a2ad73f932a3c5664
Mart6DeLavi <128328679+Mart6DeLavi@users.noreply.github.com>
2024-08-27 10:09:31 +0200

    Test Commit

---
+++ OrderService/src/test/java/com/logitrack/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logitrack.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
