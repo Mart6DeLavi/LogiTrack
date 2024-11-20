f435fc15d709f6e035cb4eef173de954f50178dc
Mart6DeLavi <128328679+Mart6DeLavi@users.noreply.github.com>
2024-11-20 14:36:23 +0100

    Rename directory from 'OrderService' and 'InventoryService' to 'logitrack-order-service' and 'logitrack-inventory-service'

---
+++ logitrack-order-service/src/main/java/com/logitrack/orderservice/dtos/UserOrderCreationDto.java
@@ -0,0 +1,15 @@
+package com.logitrack.orderservice.dtos;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserOrderCreationDto {
+    int productId;
+}
